d1d6503030556d17c286ccd9fbe7aca9d2ed938b
Hamza Sohail <102827735+Hamzasohail99@users.noreply.github.com>
2022-10-04 14:19:11 +0000

    Add files via upload

---
+++ shell-utility/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+   char	*arglist[4];
+   arglist[0] = "ls";
+   arglist[1] = "-l";
+   arglist[2] = "/home";
+   arglist[3] = NULL;
+   execvp(arglist[0], arglist);
+   return 0;
+}
